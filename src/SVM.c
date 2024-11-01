b48a3ed653b8b17d1ce9461d494af38c266f07eb
miguelfrndz <miguel.fretana@gmail.com>
2024-11-01 17:35:19 +0100

    Implemented Mini-Batch Pegasos SVM

--- src/SVM.c
+++ src/SVM.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <time.h>
 #include "utils.h"
 
 #ifdef __CUDACC__
@@ -62,6 +63,46 @@ void trainPegasosSVM(float *weights, Dataset trainData, float lambda, int iterat
     }
 }
 
+void trainBatchedPegasosSVM(float *weights, Dataset trainData, float lambda, int iterations, int batch_size) {
+    for (int step = 1; step < (iterations + 1); step++) {
+        #ifdef DEBUG
+            // Print the first weight every 1000 iterations
+            if (step % 1000 == 0) {
+                printf("Weight 0 at Iteration: %d, Weight: %f\n", step, weights[0]);
+            }
+        #endif
+        // Select batch_size random instances
+        int *batch_indices = (int *)malloc(batch_size * sizeof(int));
+        for (int i = 0; i < batch_size; i++) {
+            batch_indices[i] = rand() % trainData.instances;
+        }
+        float step_size = 1.0 / (lambda * step);
+        float *sum_positive_instances = (float *)calloc(trainData.features, sizeof(float));
+        for (int i = 0; i < batch_size; i++) {
+            int *x = trainData.input[batch_indices[i]];
+            int y = trainData.output[batch_indices[i]];
+            if (y * dotProduct(weights, x, trainData.features) < 1) {
+                for (int j = 0; j < trainData.features; j++) {
+                    sum_positive_instances[j] += y * x[j];
+                }
+            }
+        }
+        for (int i = 0; i < trainData.features; i++) {
+            weights[i] = (1 - step_size * lambda) * weights[i] + (step_size / batch_size) * sum_positive_instances[i];
+        }
+        // Gradient-Projection step
+        float ball_radius = 1.0 / sqrt(lambda);
+        float norm = 0;
+        for (int i = 0; i < trainData.features; i++) {
+            norm += weights[i] * weights[i];
+        }
+        float scaling_factor = fmin(1.0, ball_radius / sqrt(norm));
+        for (int i = 0; i < trainData.features; i++) {
+            weights[i] *= scaling_factor;
+        }
+    }
+}
+
 void predictPegasosSVM(int *predictions, float *weights, Dataset testData) {
     for (int inst = 0; inst < testData.instances; inst++) {
         int *x = testData.input[inst];
@@ -84,6 +125,24 @@ const static char *TEST_DATA;
 
 int main(int argc, char **argv) {
     (void)argc, (void)argv;
+    // Print the running device
+    #ifdef __CUDACC__
+        int threads_per_block = 256;
+        int blocks = 1;
+        // Launch the kernel
+        print_gpu_running<<<blocks, threads_per_block>>>();
+        // Check for kernel launch errors
+        cudaError_t error = cudaGetLastError();
+        if (error != cudaSuccess) {
+            fprintf(stderr, "CUDA error: %s\n", cudaGetErrorString(error));
+            return 1;
+        }
+        // Wait for GPU to finish before accessing results
+        cudaDeviceSynchronize();
+    #else
+        print_cpu_running();
+    #endif
+
     // Check if argument is mush or rcv1 and load the different datasets
     if (argc != 2) {
         fprintf(stderr, "Usage: %s <dataset>\n", argv[0]);
@@ -105,6 +164,8 @@ int main(int argc, char **argv) {
         assert(trainData.features == testData.features && "Number of features in the training and testing datasets should be the same");
         int features = trainData.features;
         printf("Number of features: %d\n", features);
+        printf("\nMethod 1: Stochastic Pegasos SVM\n");
+        clock_t start = clock();
         // Initialize the weights to zero
         float *weights = (float *)calloc(features, sizeof(float));
         // Initalize the regularization lambda param to 2*10^-4
@@ -127,11 +188,45 @@ int main(int argc, char **argv) {
             }
         }
         float accuracy = (float)correct / testData.instances;
+        clock_t end = clock();
         printf("Accuracy: %.2f\n", accuracy);
-
-        // Free the allocated memory
+        printf("Time taken: %.4f seconds\n", (double)(end - start) / CLOCKS_PER_SEC);
         free(weights);
         free(predictions);
+
+        printf("\nMethod 2: Mini-Batch Pegasos SVM\n");
+        start = clock();
+        // Initialize the weights to zero
+        weights = (float *)calloc(features, sizeof(float));
+        // Regularization lambda parameter
+        lambda = 2e-4;
+        // Number of iterations
+        iterations = 10000;
+        // Batch size for the Mini-Batch Pegasos Algorithm
+        int batch_size = 124;
+        // Train the SVM model using the Pegasos Algorithm
+        trainBatchedPegasosSVM(weights, trainData, lambda, iterations, batch_size);
+        // Make predictions
+        predictions = (int *)malloc(testData.instances * sizeof(int));
+        predictPegasosSVM(predictions, weights, testData);
+        // Evaluate the predictions
+        correct = 0;
+        for (int i = 0; i < testData.instances; i++) {
+            #ifdef DEBUG
+                printf("Prediction: %d, Actual: %d\n", predictions[i], testData.output[i]);
+            #endif
+            if (predictions[i] == testData.output[i]) {
+                correct++;
+            }
+        }
+        accuracy = (float)correct / testData.instances;
+        end = clock();
+        printf("Accuracy: %.2f\n", accuracy);
+        printf("Time taken: %.4f seconds\n", (double)(end - start) / CLOCKS_PER_SEC);
+        free(weights);
+        free(predictions);
+
+        // Free the allocated memory
         freeDataset(trainData);
         freeDataset(testData);
 
@@ -143,22 +238,6 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    #ifdef __CUDACC__
-        int threads_per_block = 256;
-        int blocks = 1;
-        // Launch the kernel
-        print_gpu_running<<<blocks, threads_per_block>>>();
-        // Check for kernel launch errors
-        cudaError_t error = cudaGetLastError();
-        if (error != cudaSuccess) {
-            fprintf(stderr, "CUDA error: %s\n", cudaGetErrorString(error));
-            return 1;
-        }
-        // Wait for GPU to finish before accessing results
-        cudaDeviceSynchronize();
-    #else
-        print_cpu_running();
-    #endif
     return 0;
 }
 
